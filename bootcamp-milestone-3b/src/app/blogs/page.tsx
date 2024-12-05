bf84f1ebdd1e6266d7a625a897ad945d0f1a623f
Amanda Yu <amandayu255@gmail.com>
2024-12-05 01:31:56 -0800

    feat: added link to access blog slugs

--- bootcamp-milestone-3b/src/app/blogs/page.tsx
+++ bootcamp-milestone-3b/src/app/blogs/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import connectDB from "@/app/database/db";
 import Blog from "@/app/database/blogSchema";
+import Link from "next/link";
 import "./Blogs.css";
 
 async function getBlogs() {
@@ -23,11 +24,13 @@ export default async function BlogsPage() {
       <div className="blog-list">
         {blogs.length > 0 ? (
           blogs.map((blog) => (
-            <div className="blog-item" key={blog.slug}>
-              <h2>{blog.title}</h2>
-              <p>{blog.description}</p>
-              <img src={blog.image} alt={blog.image_alt} />
-            </div>
+            <Link href={`/blogs/${blog.slug}`} key={blog.slug}>
+              <div className="blog-item">
+                <h2>{blog.title}</h2>
+                <p>{blog.description}</p>
+                <img src={blog.image} alt={blog.image_alt} />
+              </div>
+            </Link>
           ))
         ) : (
           <p>No blogs found.</p>
