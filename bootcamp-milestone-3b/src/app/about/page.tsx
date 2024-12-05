06d6af8b057633a34c81e0c2c0cd927fdebfa270
Amanda Yu <amandayu255@gmail.com>
2024-12-04 22:12:51 -0800

    chore: copied files from milestone 3a into milestone 3b

---
+++ bootcamp-milestone-3b/src/app/about/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+export default function About() {
+  return (
+    <div>
+      <h1 className="page-title">Contact</h1>
+
+      <form id="contact-form">
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" />
+        <br />
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+        <br />
+
+        <label htmlFor="message">Message:</label>
+        <textarea id="message" name="message" required></textarea>
+        <br />
+
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
